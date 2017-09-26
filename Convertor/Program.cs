330c96ab1588f5ef055bd1b460e28f2acf7c71e4
Raluca Baros <rbaros@endava.com>
2017-09-26 16:47:27 +0300

    Add project files.

---
+++ Convertor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Convertor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter the file path.");
+            }
+
+            var filePath = args[0];
+            var hexaResult= ConvertFileToHexString(filePath, ",");
+            Clipboard.SetText(hexaResult);
+            
+            Console.Read();
+        }
+
+        public static string ConvertFileToHexString(string filePath, string delimiter)
+        {
+            byte[] byteArray = File.ReadAllBytes(filePath);
+            string[] stringArray = byteArray.Select(p => p.ToString("x2")).ToArray();
+
+            var stringBuilder = new StringBuilder(stringArray.Length * 2);
+            foreach (var value in stringArray)
+            {
+                stringBuilder.AppendFormat("{0}{1}", "0x", value);
+                stringBuilder.Append(delimiter);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
